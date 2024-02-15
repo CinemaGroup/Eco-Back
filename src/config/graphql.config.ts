6bf6b9c205d16aa105f99686ede5c70664965e0b
Prizmahosting <mosyan.gurgen.hosting@mail.ru>
2024-02-15 16:31:56 +0400

    first commit

--- src/config/graphql.config.ts
+++ src/config/graphql.config.ts
@@ -1,12 +1,12 @@
 import { ApolloDriverConfig } from '@nestjs/apollo'
 import { ConfigService } from '@nestjs/config'
 import { join } from 'path'
-import { isDev } from 'src/utils/is-dev.util'
 
 export const getGraphQLConfig = async (
 	configService: ConfigService
 ): Promise<ApolloDriverConfig> => ({
-	playground: isDev(configService),
+	path: '/mygraphql',
+	playground: true,
 	autoSchemaFile: join(process.cwd(), 'src/scheme/scheme.gql'),
 	context: ({ req, res }) => ({
 		req,
